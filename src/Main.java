afc0ed7c6ff89421d54f32ae4535ae99b1038003
Rana emara <schleidenjakob@gmail.com>
2023-11-05 11:45:34 +0200

    2nd

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,87 @@ public class Main {
             System.out.println("Choose an option:");
             System.out.println("1. Compress from console input");
             System.out.println("2. Decompress from console input");
-            System.out.println("3. Compress from file to file (same path and name)");
-            System.out.println("4. Decompress from file to file (same path and name)");
+            System.out.println("3. Compress from file ");
+            System.out.println("4. Decompress from file");
             System.out.println("5. Exit");
             System.out.print("Enter your choice (1/2/3/4/5): ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-
+            if (choice == 1) {
+                System.out.print("Enter the text to compress: ");
+                String input = scanner.nextLine();
+                String compressedData = compression.compress(input);
+                System.out.println("Compressed Data: " + compressedData);
+            }  else if (choice == 2) {
+                System.out.print("Enter the compressed data as a list of integers separated by commas: ");
+                String compressedDataInput = scanner.nextLine();
+                String decompressedData = decompression.decompress(compressedDataInput);
+                System.out.println("Decompressed Data: " + decompressedData);
+            }
+            else if (choice == 3) {
+                compressFromFile();
+            } else if (choice == 4) {
+                decompressToFile();
+            } else if (choice == 5) {
+                System.out.println("Exiting the program.");
+                break;
+            } else {
+                System.out.println("Invalid choice. Please choose a valid option.");
+            }
         }
 
+        scanner.close();
+    }
 
+    private static void compressFromFile() {
+        try {
+            File file = new File("java.txt");
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            StringBuilder fileContent = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+
+                fileContent.append(line);
+
+
+            }
+            String strcon=fileContent.toString();
+            String compressedData = compression.compress(strcon);
+//            String compressedDataStr = compressedData.toString();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, false));
+            writer.write(compressedData);
+            writer.newLine();
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void decompressToFile() {
+        try {
+            File file = new File("java.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            StringBuilder fileContent = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line);
+
+            }
+            String strcon=fileContent.toString();
+            String decompressedData = decompression.decompress(strcon);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, false));
+            writer.write(decompressedData);
+            writer.newLine();
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/decompression.java
+++ src/decompression.java
@@ -1,18 +1,21 @@
-
 import java.util.*;
 import java.io.*;
-class decompression {
-    public static String decompress(List<Integer> compressed) {
+
+public class decompression {
+    public static String decompress(String compressedInput) {
+        List<Integer> compressedData = parse(compressedInput);
+
         HashMap<Integer, String> dictionary = new HashMap<>();
         for (int i = 0; i < 128; i++) {
-            dictionary.put(i, Character.toString((char) i));
+            dictionary.put(i, Character.toString(i));
         }
 
         int currentCode = 128;
-        String previous = Character.toString((char)(int)compressed.remove(0));
+        String previous = Character.toString(compressedData.remove(0));
+
         StringBuilder decompressed = new StringBuilder(previous);
 
-        for (int code : compressed) {
+        for (int code : compressedData) {
             String entry;
             if (dictionary.containsKey(code)) {
                 entry = dictionary.get(code);
@@ -28,5 +31,23 @@ class decompression {
         }
 
         return decompressed.toString();
+    }
+
+    public static List<Integer> parse(String input) {
+        List<Integer> compressedData = new ArrayList<>();
+        String result=input.substring(1,input.length()-1);
+        String[] values = result.split(", ");
+
+        for (String value : values) {
+            compressedData.add(Integer.parseInt(value.trim()));
+        }
+
+        return compressedData;
+    }
+
+    public static void main(String[] args) {
+        String inputString = "[65, 66, 65, 128, 128, 129, 131, 134, 130, 129, 66, 138, 139, 138]";
+        String decompressedData = decompress(inputString);
+        System.out.println(decompressedData);
     }
 }
